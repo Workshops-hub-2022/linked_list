54b9e12eba7189029d69f5b624692d51e121bc83
Nathan <nathan.barre@epitech.eu>
2022-11-07 18:34:12 +0100

    add: free list

--- src/free_list.c
+++ src/free_list.c
@@ -9,21 +9,41 @@
 
 void free_element(list li)
 {
-    
+    li->next = NULL;
+    free(li);
 }
 
 list pop_elem_back(list li)
 {
+    list temp = li;
 
+    if (is_empty_list(li)) {
+        free_element(li);
+        return (NULL);
+    }
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    free_element(temp);
+
+    return (li);
 }
 
 list pop_elem_front(list li)
 {
+    list new = li->next;
 
+    free_element(li);
+
+    return (new);
 }
 
 void clear_list(list li)
 {
-
+    while (li != NULL) {
+        li = pop_elem_front(li);
+    }
 }
 
